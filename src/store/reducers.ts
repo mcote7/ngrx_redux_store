c45fc00a504d68e7588286e35703d922112af6dd
Michael Cote <michaelcote7@outlook.com>
2021-07-25 14:26:42 -0700

    .

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -8,27 +8,26 @@ export const initialState = {
 
 export function reducer(
   state = initialState,
-  action: { type: string; payload: any }
-  ) {
-    switch(action.type) {
-      
-      case fromActions.ADD_TODO: {
-        const todo = action.payload;
-        const data = [...state.data, todo];
-        return {
-          ...state,
-          data // data: data
-        };
-      }
-      
-      case fromActions.REMOVE_TODO: {
-        const todoX = action.payload.label;
-        const data = state.data.filter(todo => todo.label !== todoX);
-        return {
-          ...state,
-          data
-        };
-      }
+  action: { type: string; payload: any } ) {
+  switch(action.type) {
+    
+    case fromActions.ADD_TODO: {
+      const todo = action.payload;
+      const data = [...state.data, todo];
+      return {
+        ...state,
+        data // data: data
+      };
     }
-    return state;
-  };
+    
+    case fromActions.REMOVE_TODO: {
+      const todoX = action.payload.label;
+      const data = state.data.filter(todo => todo.label !== todoX);
+      return {
+        ...state,
+        data
+      };
+    }
+  }
+  return state;
+};
